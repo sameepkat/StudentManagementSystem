17f34003d9cb7938f0b4b3989c4e9e9844b57b8d
Sameep <sameepsk2@gmail.com>
2025-02-03 12:04:48 +0545

    reset

---
+++ apps/client/src/App.tsx
@@ -0,0 +1,12 @@
+// import { useState } from 'react'
+import "./App.css";
+
+function App() {
+  return(
+    <>
+      <h1 className='underline italic'>Hello World Underlined</h1>
+    </>
+  )
+}
+
+export default App;
