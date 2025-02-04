92bf2158ec62d3aa3fdffc132b55d47f0c545186
Sameep <sameepsk2@gmail.com>
2025-02-04 19:21:18 +0545

    login 2

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,26 +1,25 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import "./App.css";
 import LoginCard from "./components/login";
-import TestComponent from "./components/test"
+import TestComponent from "./components/test";
 import NoMatch from "./components/noMatch";
 
-function TempHome(){
-  return(
-    <h1 className="flex justify-center italic ">This is the home page</h1>
-  )
-}
-
 function App() {
-  return(
-      <Router>
-        <Routes>
-          <Route path="/" element={<TempHome />} />
-          <Route path="/login" element={<LoginCard />} />
-          <Route path="/test" element={<TestComponent />} />
-          <Route path="*" element={<NoMatch />} />
-        </Routes>
-      </Router>
-  )
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<LoginCard />} />
+        <Route path="/test" element={<TestComponent />} />
+        <Route path="*" element={<NoMatch />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- apps/client/src/components/login.tsx
+++ apps/client/src/components/login.tsx
@@ -1,13 +1,40 @@
-
-function LoginCard(){
-    return (
-    <>       
-        <div className="flex font-bold border justify-center relative flex-1 shadow-xs">
-            <h2>This is the login Page</h2>
-        </div>
+function LoginCard() {
+  return (
+    <>
+      <div className="w-screen h-screen ">
+        <button className="m-3 px-3 py-3 bg-green-500 text-white cursor-pointer hover:bg-green-700 rounded-sm">
+          Dark Mode
+        </button>
+        <main className="flex flex-col justify-center items-center ">
+          <div className="w-[400px] h-[30%] flex flex-col justify-center items-center border border-gray-200 rounded-md shadow-2xl">
+            <img
+              src="/login_PU_logo.png"
+              className="p-10 "
+              alt="Pokhara University"
+            />
+            <span className="text-blue-800 mb-4">Sign in to access SMS</span>
+            <input
+              type="text"
+              name="username"
+              id="usernameInput"
+              placeholder="Username"
+              className="block border border-gray-400 px-3 py-2 my-2 w-3/4"
+            />
+            <input
+              type="password"
+              name="password"
+              id="passwordInput"
+              placeholder="Password"
+              className="block border border-gray-400 px-3 py-2 my-2 w-3/4"
+            />
+            <button className="py-2 px-5 bg-green-500 rounded-sm text-white my-2 cursor-pointer">
+              Login
+            </button>
+          </div>
+        </main>
+      </div>
     </>
-
-    )
+  );
 }
 
 export default LoginCard;
